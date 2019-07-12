17c52a29c47739f04701f74abddf97fca651eca1
unknown <uchpolinet@gmail.com>
2019-07-12 23:26:31 +1000

    edited HTML webpage using bootstrap and CSS and assigned the images buttons with the values in javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$("#linkBtn").attr(value);
+$("#miphaBtn").attr(value);
+$("#lynelBtn").attr(value);
+$("#darkBtn").attr(value);
+
+// function to start the game
+// chosen character moves to "Your Character"
+// other characters fall into enemies
+// when attack button is pressed, both player and enemy's value decrease
+function startGame() {}
+
+function restartGame() {}
