deaa2aa045de63eca0d4e3cc374435c094592268
unknown <uchpolinet@gmail.com>
2019-07-13 21:57:10 +1000

    added more styling and pseudocoded the javascript game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,29 @@
-$("#linkBtn").attr(value);
-$("#miphaBtn").attr(value);
-$("#lynelBtn").attr(value);
-$("#darkBtn").attr(value);
+$(document).ready(function() {
+  var characterChosenByUser = "";
+  var healthPoint = 10;
+  var attackPower = "";
+  var countAttackPower = "";
 
-// function to start the game
-// chosen character moves to "Your Character"
-// other characters fall into enemies
-// when attack button is pressed, both player and enemy's value decrease
-function startGame() {}
+  $("#linkBtn").attr(value);
+  $("#miphaBtn").attr(value);
+  $("#lynelBtn").attr(value);
+  $("#darkBtn").attr(value);
 
-function restartGame() {}
+  // onclick function - when user select a character move to a different div
+  $("#chosenCharacter").on("click", function() {
+    var newDiv = $("#chosenCharacter");
+    $("#linkBtn").append("<div></div>");
+  });
+
+  // when a character is selected, move to an HTML element with an id "chosenCharacter"
+
+  // remaining characters are moved to an HTML element with an id "displayEnemies"
+
+  // player select an emeny to find and move the selected character to HTML element with an id "defender"
+
+  // attack power starts counting when play clicks on attack button
+
+  // when attack button is triggered each character losses their attack power
+  // the increment of attack power base is 6
+  // the decrement of attack power base is 3
+});
